79f7ceb829b8cd8a40ff7d48782da2260746cde8
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-09-30 13:47:30 +0100

    Add files via upload

---
+++ libft/ft_strcmpall.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmpall.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sghajdao <sghajdao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/30 13:41:07 by sghajdao          #+#    #+#             */
+/*   Updated: 2022/09/30 13:44:28 by sghajdao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libc.h"
+
+int ft_strcmpall(char *str, int start, int c)
+{
+    while (str[start] && str[start] == c)
+        start++;
+    if (str[start] != '\0')
+        return (1);
+    else
+        return (0);
+}
