01dc54007fd024db2fc0a49197437eef0ef7d843
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-09-30 14:34:12 +0100

    Update ft_strcmpall.c

--- libft/ft_strcmpall.c
+++ libft/ft_strcmpall.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 int ft_strcmpall(char *str, int start, int c)
 {
