68a1b105928ebbb03913b0835f8847bc3c7e6482
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-10-08 12:32:45 +0100

    Update expander.c

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -37,8 +37,10 @@ char	*expander(char *arg, t_env *env, int ret)
 	t_expander	ex;
 	int			new_arg_len;
 
+	if (!arg)
+		return (NULL);
 	new_arg_len = cmdsLenForAlloc(arg, env, ret);
-	if (!(ex.new_arg = malloc(sizeof(char) * new_arg_len + 1)))
+	if (!(ex.new_arg = malloc(BUFF_SIZE)))
 		return (NULL);
 	ex.i = 0;
 	ex.j = 0;
