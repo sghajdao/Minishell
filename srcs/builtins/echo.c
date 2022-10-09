9ef06fe49f618d3f27d14a612ab5d2fb6cf262fb
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-10-09 07:19:43 +0100

    Update echo.c

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -4,34 +4,46 @@ static	int	how_many_args(char **args)
 {
 	int	size;
 
+	if (!args || !*args)
+		return (0);
 	size = 0;
 	while (args[size])
 		size++;
 	return (size);
 }
 
+static int	write_echo_args(char **args)
+{
+	int	i;
+	int	n_option;
+
+	i = 1;
+	n_option = 0;
+	while (args[i] && (ft_strcmp(args[i], "-n") == 0 \
+		|| ft_strcmpall(args[i], 1, 'n') == 0))
+	{
+		n_option = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1] && args[i][0] != '\0')
+			write(1, " ", 1);
+		i++;
+	}
+	return (n_option);
+}
+
 int	ft_echo(char **args)
 {
 	int	n_option;
-	int	i;
 
 	n_option = 0;
-	i = 1;
+	if (!args || !*args)
+		return (1);
 	if (how_many_args(args) > 1)
-	{
-		while (args[i] && (ft_strcmp(args[i], "-n") == 0 || ft_strcmpall(args[i], 1, 'n') == 0))
-		{
-			n_option = 1;
-			i++;
-		}
-		while (args[i])
-		{
-			ft_putstr_fd(args[i], 1);
-			if (args[i + 1] && args[i][0] != '\0')
-				write(1, " ", 1);
-			i++;
-		}
-	}
+		n_option = write_echo_args(args);
 	if (n_option == 0)
 		write(1, "\n", 1);
 	return (SUCCESS);
