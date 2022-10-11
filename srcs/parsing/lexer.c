bf5cbd3dcbdffd72fbf4a02b95376e47d51f3a28
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-10-11 11:34:03 +0100

    cmd fixie

--- srcs/parsing/lexer.c
+++ srcs/parsing/lexer.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-char	*alloc_space(char *line)
+char	*alloc_space(char *line, t_mini *mini)
 {
 	char	*new;
 	int		i;
@@ -10,7 +10,7 @@ char	*alloc_space(char *line)
 	i = 0;
 	while (line[i])
 	{
-		if (ft_isseparator(line, i))
+		if (ft_isseparator(line, i, mini))
 			count++;
 		i++;
 	}
@@ -19,7 +19,7 @@ char	*alloc_space(char *line)
 	return (new);
 }
 
-char	*addSpaceBetweenCmds(char *line)
+char	*addSpaceBetweenCmds(char *line, t_mini *mini)
 {
 	char	*new;
 	int		i;
@@ -27,14 +27,14 @@ char	*addSpaceBetweenCmds(char *line)
 
 	i = 0;
 	j = 0;
-	new = alloc_space(line);
+	new = alloc_space(line, mini);
 	while (new && line[i])
 	{
-		if (isQuoteOpen(line, i) == 0 && ft_isseparator(line, i))
+		if (isQuoteOpen(line, i, mini) == 0 && ft_isseparator(line, i, mini))
 		{
 			new[j++] = ' ';
 			new[j++] = line[i++];
-			if (isQuoteOpen(line, i) == 0 && (line[i] == '>' || line[i] == '<'))
+			if (isQuoteOpen(line, i, mini) == 0 && (line[i] == '>' || line[i] == '<'))
 				new[j++] = line[i++];
 			new[j++] = ' ';
 		}
@@ -46,9 +46,9 @@ char	*addSpaceBetweenCmds(char *line)
 	return (new);
 }
 
-int		check_quote(t_mini *mini, char **line)
+int	check_quote(t_mini *mini, char **line)
 {
-	if (isQuoteOpen(*line, 2147483647))
+	if (isQuoteOpen(*line, 2147483647, mini))
 	{
 		ft_putendl_fd("minishell: syntax error with open quotes", STDERR);
 		ft_memdel(*line);
@@ -69,8 +69,24 @@ char    *lexer(t_mini *mini)
 		add_history(line);
 	else if (!line)
 		run_signals(3);
+	if (g_sig.sigint == 1)
+		mini->ret = g_sig.exit_status;
 	if (check_quote(mini, &line))
 		return (NULL);
-	line = addSpaceBetweenCmds(line);
+	line = addSpaceBetweenCmds(line, mini);
     return (line);
 }
+
+
+// ls | cat | wc
+
+/**
+ * @brief 
+ * 
+ * ls  |  cat  | wc
+ * 
+ * ls => comannde
+ * | pipe
+ * cat 
+ * cat << end
+ */
