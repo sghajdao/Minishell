88742b639afb714c0a792c74402da37418e28a7c
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-10-11 15:53:29 +0100

    new file

---
+++ srcs/env/env_tools.c
@@ -0,0 +1,47 @@
+#include "minishell.h"
+
+size_t	envlen(t_env *lst)
+{
+	size_t	lst_len;
+
+	lst_len = 0;
+	while (lst && lst->next != NULL)
+	{
+		if (lst->value != NULL)
+		{
+			lst_len += ft_strlen(lst->value);
+			lst_len++;
+		}
+		lst = lst->next;
+	}
+	return (lst_len);
+}
+
+char	*env_to_string(t_env *lst)
+{
+	int		i;
+	int		j;
+	char	*env;
+
+	if (!(env = malloc(BUFF_SIZE)))
+		return (NULL);
+	i = 0;
+	while (lst && lst->next != NULL)
+	{
+		if (lst->value != NULL)
+		{
+			j = 0;
+			while (lst->value[j])
+			{
+				env[i] = lst->value[j];
+				j++;
+				i++;
+			}
+		}
+		if (lst->next->next != NULL)
+			env[i++] = '\n';
+		lst = lst->next;
+	}
+	env[i] = '\0';
+	return (env);
+}
