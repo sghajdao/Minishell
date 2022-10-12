403de31033c2ebdaa4540f186d80bff6230f7ff3
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-10-12 17:14:38 +0100

    Create ft_counter.c

---
+++ libft/ft_counter.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_counter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sghajdao <sghajdao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 17:14:14 by sghajdao          #+#    #+#             */
+/*   Updated: 2022/10/12 17:14:28 by sghajdao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_counter(char *str, int c)
+{
+    size_t  count;
+    int     i;
+
+    if (!str)
+        return (0);
+    i = 0;
+    count = 0;
+    while (str[i])
+    {
+        if (str[i] == c)
+            count++;
+        i++;
+    }
+    return (count);
+}
