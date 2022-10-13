4d606ca9107ea49df31b35c8b2a9f4c5e154c0dc
Saad Alaoui <95027314+sghajdao@users.noreply.github.com>
2022-10-13 07:24:06 +0100

    Update sort_env.c

--- srcs/env/sort_env.c
+++ srcs/env/sort_env.c
@@ -51,10 +51,14 @@ void	add_quotes_to_value(char **tab)
 			write(1, &tab[i][j], 1);
 			j++;
 		}
-		write(1, "=\"", 2);
-		while (tab[i][++j])
-			write(1, &tab[i][j], 1);
-		write(1, "\"\n", 2);
+		if (ft_search(tab[i], '=') == 1)
+		{
+			write(1, "=\"", 2);
+			while (tab[i][++j])
+				write(1, &tab[i][j], 1);
+			write(1, "\"", 1);
+		}
+		write(1, "\n", 1);
 		i++;
 	}
 }
