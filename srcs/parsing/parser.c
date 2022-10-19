d7273e65ac616a864007177605a02741af16cdb3
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-10-19 12:09:35 +0100

    Update parser.c

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sghajdao <sghajdao@student.42.fr>          +#+  +:+       +#+        */
+/*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 12:22:36 by sghajdao          #+#    #+#             */
-/*   Updated: 2022/10/18 10:07:39 by sghajdao         ###   ########.fr       */
+/*   Updated: 2022/10/18 21:54:09 by slammari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	parser(t_mini *mini)
 	while (token)
 	{
 		if (ft_istype(token, ARG))
-			type_arg_parsing(token, 0);
+			type_arg_parsing(token);
 		token = token->next;
 	}
 }
